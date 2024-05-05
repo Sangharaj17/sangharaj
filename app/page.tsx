063118669d6735d65b80ccaa5c63dcb74aad8a41
MonkSangha <monksangharaj@gmail.com>
2024-05-05 20:20:53 +0530

    responsive changes I

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 import Hero from "@/components/Hero/Hero";
 export default function Home() {
   return (
-    <main className="px-12 relative min-h-screen ">
+    <main className="md:px-12 px-0 relative min-h-screen ">
       <div className="flex min-h-screen ">
         <div className="fixed min-h-screen flex items-center">
-          <Sidebar />
+          <div className="fixed-sidebar md:border-0 border-t">
+            <Sidebar />
+          </div>
         </div>
         <div className="w-full flex flex-col">
           <Navbar />
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className='w-full flex justify-between items-center pt-10'>
+        <div className='w-full flex justify-between items-center md:pt-10 py-4 md:py-0 md:px-0 px-4 border-b md:border-none'>
             <div className='text-lg font-semibold'>
                 ZenCoder
             </div>
-            <div className='flex items-center justify-center gap-8'>
+            <div className='hidden md:flex items-center justify-center gap-8'>
                 <Link href=''>
                     Home
                 </Link>
@@ -21,10 +21,13 @@ function Navbar() {
                     Projects
                 </Link>
             </div>
-            <div className=''>
+            <div className='hidden md:block'>
                 <Link href='' className='p-4 border rounded-lg '>
                     Let's Chat
                 </Link>
+            </div>
+            <div className='md:hidden block'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-align-right"><line x1="21" x2="3" y1="6" y2="6" /><line x1="21" x2="9" y1="12" y2="12" /><line x1="21" x2="7" y1="18" y2="18" /></svg>
             </div>
         </div>
     )
